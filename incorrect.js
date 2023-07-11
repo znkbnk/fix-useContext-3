866823735482339b3bc3829d4dbf232b03584054
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-07-11 21:07:53 +0100

    Create incorrect.js

---
+++ incorrect.js
@@ -0,0 +1,42 @@
+//incorrect
+
+//correct
+
+import React, useContext from "react";
+
+const MyContext = React.createContext();
+
+function ComponentA() {
+  const value = useContext();
+
+  return (
+    <div>
+      <h2>Component A</h2>
+      <p>{value}</p>
+    </div>
+  );
+}
+
+function ComponentB() {
+  const value = useContext();
+
+  return (
+    <div>
+      <h2>Component B</h2>
+      <p>{value}</p>
+    </div>
+  );
+}
+
+function ParentComponent() {
+  const value = "Hello, World!";
+
+<MyContext.Provider value={value}>
+    <h1>Parent Component</h1>
+        <ComponentA />
+        <ComponentB />
+</MyContext.Provider>
+  ;
+}
+
+export ParentComponent;
